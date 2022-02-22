07cb9c49b5aa5d5cb77e1b2a72fae8086195651b
colin-leeyc <90929720+colin-leeyc@users.noreply.github.com>
2022-02-22 17:30:04 +0000

    Add files via upload

--- R/pseudobulk_functions.R
+++ R/pseudobulk_functions.R
@@ -10,7 +10,6 @@
 #'
 #' @return: list of Pseudobulk raw counts, DESeq2 object, VST-transformed matrix, DE results, colData design table
 #' @export
-#' 
 runPseudoBulk <- function(sce, 
                           cellIDs = NULL, 
                           obs,
@@ -115,7 +114,6 @@ runPseudoBulk <- function(sce,
 #' 
 #' @return A list of LogFoldChange, adjusted P value, and p value statistics for genes of interest
 #' @export
-#' 
 iteratePseudoBulk <- function(sce, 
                               cellIDs = NULL, 
                               obs,
@@ -181,7 +179,6 @@ iteratePseudoBulk <- function(sce,
 #' This is an internal function used by iteratePseudoBulk
 #' @return A list of LogFoldChanges, adjusted P values, and p values for genes of interest
 #' @export
-#' 
 resultsPseudoBulk <- function(sce_int, 
                               cellIDs_int = NULL, 
                               obs_int,
